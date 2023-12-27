59aa7745abf2fd713785d86305c4dd974f3526d5
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2023-12-27 18:57:48 +0900

    CreateFontFromLOGFONT

--- src/gdi_interop.rs
+++ src/gdi_interop.rs
@@ -20,10 +20,21 @@ pub fn convert_font_to_logfont(
     Ok((logfont, is_system_font.as_bool()))
 }
 
+pub fn create_font_from_logfont(
+    interop: &IDWriteGdiInterop,
+    logfont: &LOGFONTW,
+) -> anyhow::Result<IDWriteFont> {
+    unsafe { Ok(interop.CreateFontFromLOGFONT(logfont)?) }
+}
+
 pub struct GdiInterop(pub IDWriteGdiInterop);
 
 impl GdiInterop {
     pub fn convert_font_to_logfont(&self, font: &Font) -> anyhow::Result<(LOGFONTW, bool)> {
         convert_font_to_logfont(&self.0, &font.0)
+    }
+
+    pub fn create_font_from_logfont(&self, logfont: &LOGFONTW) -> anyhow::Result<IDWriteFont> {
+        create_font_from_logfont(&self.0, logfont)
     }
 }
