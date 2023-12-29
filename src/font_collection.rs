fd7fe81047ec207d2f7f99b3a58a86e06f5135de
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2023-12-30 03:35:59 +0900

    impl iter

--- src/font_collection.rs
+++ src/font_collection.rs
@@ -23,3 +23,46 @@ impl FontCollection {
         get_font_family(&self.0, index).map(FontFamily)
     }
 }
+
+pub struct FontCollectionIter<'a> {
+    collection: &'a FontCollection,
+    index: usize,
+}
+
+impl<'a> Iterator for FontCollectionIter<'a> {
+    type Item = FontFamily;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.len() {
+            let family = self.collection.get_font_family(self.index as u32);
+            self.index += 1;
+            match family {
+                Ok(family) => Some(family),
+                Err(e) => {
+                    eprintln!("{}", e);
+                    assert!(false);
+                    None
+                }
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> ExactSizeIterator for FontCollectionIter<'a> {
+    #[inline]
+    fn len(&self) -> usize {
+        self.collection.get_font_family_count() as usize
+    }
+}
+
+impl<'a> IntoIterator for &'a FontCollection {
+    type Item = FontFamily;
+    type IntoIter = FontCollectionIter<'a>;
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter {
+            collection: self,
+            index: 0,
+        }
+    }
+}
--- src/font_family.rs
+++ src/font_family.rs
@@ -30,3 +30,46 @@ impl FontFamily {
         get_font(&self.0, index).map(Font)
     }
 }
+
+pub struct FontFamilyIter<'a> {
+    family: &'a FontFamily,
+    index: usize,
+}
+
+impl<'a> Iterator for FontFamilyIter<'a> {
+    type Item = Font;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.len() {
+            let font = self.family.get_font(self.index as u32);
+            self.index += 1;
+            match font {
+                Ok(font) => Some(font),
+                Err(e) => {
+                    eprintln!("{}", e);
+                    assert!(false);
+                    None
+                }
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> ExactSizeIterator for FontFamilyIter<'a> {
+    #[inline]
+    fn len(&self) -> usize {
+        self.family.get_font_count() as usize
+    }
+}
+
+impl<'a> IntoIterator for &'a FontFamily {
+    type Item = Font;
+    type IntoIter = FontFamilyIter<'a>;
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter {
+            family: self,
+            index: 0,
+        }
+    }
+}
