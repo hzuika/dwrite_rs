d761a6f2f0a7acf2ea1ac504428c01d404dc8779
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2024-01-24 20:46:46 +0900

    TryGetFontTable

--- src/font_face.rs
+++ src/font_face.rs
@@ -1,4 +1,7 @@
-use windows::Win32::Graphics::DirectWrite::{IDWriteFontFace, IDWriteFontFile};
+use windows::Win32::{
+    Foundation::BOOL,
+    Graphics::DirectWrite::{IDWriteFontFace, IDWriteFontFile},
+};
 
 use crate::font_file;
 
@@ -20,4 +23,48 @@ pub fn get_files(face: &IDWriteFontFace) -> anyhow::Result<IDWriteFontFile> {
 pub fn get_filepath(face: &IDWriteFontFace) -> anyhow::Result<String> {
     let file = get_files(face)?;
     font_file::get_filepath(&file)
+}
+
+pub fn map_font_table<F: FnOnce(&[u8])>(
+    face: &IDWriteFontFace,
+    tag: u32,
+    f: F,
+) -> anyhow::Result<()> {
+    let mut exists = BOOL::from(false);
+    let mut data = std::ptr::null_mut();
+    let mut size = 0_u32;
+    let mut context = std::ptr::null_mut();
+    unsafe { face.TryGetFontTable(tag, &mut data, &mut size, &mut context, &mut exists) }?;
+    if exists.as_bool() {
+        if data.is_null() {
+            assert!(false);
+            return Err(anyhow::anyhow!("data exists but is null"));
+        }
+        if size == 0 {
+            assert!(false);
+            return Err(anyhow::anyhow!("data exists but size is zero"));
+        }
+        let data = unsafe { std::slice::from_raw_parts(data as *const u8, size as usize) };
+        f(data);
+        unsafe { face.ReleaseFontTable(context) };
+    }
+    Ok(())
+}
+
+pub fn get_font_table(face: &IDWriteFontFace, tag: u32) -> anyhow::Result<Vec<u8>> {
+    let mut table = Vec::new();
+    map_font_table(face, tag, |data| {
+        table = data.to_vec();
+    })?;
+    Ok(table)
+}
+
+pub fn is_variable_font(face: &IDWriteFontFace) -> anyhow::Result<bool> {
+    let mut is_variable_font = false;
+    map_font_table(face, u32::from_le_bytes(*b"fvar"), |data| {
+        if data.len() > 0 {
+            is_variable_font = true;
+        }
+    })?;
+    Ok(is_variable_font)
 }
