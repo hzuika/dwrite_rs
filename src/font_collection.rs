3ea85f6387508ff0723648960af3b00ca5790456
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2024-02-17 00:32:02 +0900

    find family

--- src/font_collection.rs
+++ src/font_collection.rs
@@ -1,4 +1,10 @@
-use windows::Win32::Graphics::DirectWrite::{IDWriteFontCollection, IDWriteFontFamily};
+use windows::{
+    core::PCWSTR,
+    Win32::{
+        Foundation::BOOL,
+        Graphics::DirectWrite::{IDWriteFontCollection, IDWriteFontFamily},
+    },
+};
 
 use crate::font_family::FontFamily;
 
@@ -13,6 +19,33 @@ pub fn get_font_family(
     Ok(unsafe { collection.GetFontFamily(index) }?)
 }
 
+pub fn find_family_name(
+    collection: &IDWriteFontCollection,
+    family_name: &str,
+) -> anyhow::Result<(u32, bool)> {
+    unsafe {
+        let mut index = 0_u32;
+        let mut exists = BOOL::from(false);
+        let mut family_name: Vec<u16> = family_name.encode_utf16().collect();
+        family_name.push(0);
+        let family_name = PCWSTR::from_raw(family_name.as_ptr());
+        collection.FindFamilyName(family_name, &mut index, &mut exists)?;
+        Ok((index, exists.as_bool()))
+    }
+}
+
+pub fn find_family(
+    collection: &IDWriteFontCollection,
+    family_name: &str,
+) -> Option<IDWriteFontFamily> {
+    let (index, exists) = find_family_name(collection, family_name).ok()?;
+    if exists {
+        get_font_family(collection, index).ok()
+    } else {
+        None
+    }
+}
+
 pub struct FontCollection(pub IDWriteFontCollection);
 impl FontCollection {
     pub fn get_font_family_count(&self) -> u32 {
@@ -21,6 +54,10 @@ impl FontCollection {
 
     pub fn get_font_family(&self, index: u32) -> anyhow::Result<FontFamily> {
         get_font_family(&self.0, index).map(FontFamily)
+    }
+
+    pub fn find_family(&self, family_name: &str) -> Option<FontFamily> {
+        find_family(&self.0, family_name).map(FontFamily)
     }
 }
 
